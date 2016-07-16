4ffdd1c47e79a24461f3906dba573964d7012dbd
Edward P. Kelly <ed.kelly@nbcuni.com>
2016-07-16 13:40:22 -0400

    First commit.

---
+++ SwiftyCalculator/CalculatorBrain.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorBrain.swift
+//  SwiftyCalculator
+//
+//  Created by Ed Kelly on 7/14/16.
+//  Copyright © 2016 Edward P. Kelly LLC. All rights reserved.
+//
+
+import Foundation
+
+
+class CalculatorBrain {
+    
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+    
+    func performOperation(symbol: String)
+    {
+        if symbol == "π" {
+            //displayValue = M_PI
+        }
+        if symbol == "√" {
+            //displayValue = sqrt(displayValue)
+        }
+    }
+    
+}
---
+++ SwiftyCalculator/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SwiftyCalculator
+//
+//  Created by Ed Kelly on 7/14/16.
+//  Copyright © 2016 Edward P. Kelly LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var displayText: UILabel!
+    
+    private var calcBrain = CalculatorBrain()
+    private var userIsTyping = false
+    
+    private var displayValue: Double {
+        get {
+            return Double(displayText.text!)!
+        }
+        set {
+            displayText.text = String(newValue)
+        }
+    }
+
+    @IBAction private func onTouchDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            let displayContents = displayText.text!
+            displayText.text = displayContents + digit
+        } else {
+            displayText.text = digit
+        }
+        userIsTyping = true
+    }
+    
+    @IBAction private func onPerformOperation(sender: UIButton)
+    {
+        userIsTyping = false
+        if let symbol = sender.currentTitle {
+            calcBrain.performOperation(symbol)
+        }
+        
+    }
+}
+
